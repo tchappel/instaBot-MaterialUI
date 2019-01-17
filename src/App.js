3bcf948526751e6878aff0483b399598b89d59a4
Thomas Chappel <thomas.chappel4@gmail.com>
2019-01-17 23:35:13 +0100

    add material-UI npm-classnames react-router and starter template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Album from "./Album.js";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <MuiThemeProvider>
+          <div className="App">
+            <Route path="/" exact component={Album} />
+            <Route path="/login" />
+            <Route />
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
